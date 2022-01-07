b826650195b676a7227340ca7dda0a13c62d7a3b
ChenDataaa <68600402+ChenDataaa@users.noreply.github.com>
2022-01-07 21:10:36 +0800

    test

--- algorithm/algorithms/src/test.java
+++ algorithm/algorithms/src/test.java
@@ -1,5 +1,9 @@
-package PACKAGE_NAME;/**
-    @Description:
-    @create: 2022/01/07 - 21:06
-*/public class test {
+/**
+ * @Description:
+ * @create: 2022/01/07 - 21:06
+ */
+public class test {
 }
+
+
+
